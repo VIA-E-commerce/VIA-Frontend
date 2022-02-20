5b17937ab655344a28096419f02880e207ab79bb
Kyeongho Yang <88yangkh@gmail.com>
2022-02-21 01:08:27 +0900

    :wrench: Webpack 기본 설정
    Closes #2

---
+++ webpack.config.ts
@@ -0,0 +1,44 @@
+import webpack, { Configuration } from 'webpack';
+import path from 'path';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+
+const DEV_ENV = 'development';
+const PROD_ENV = 'production';
+const isDevMode = process.env.NODE_ENV !== PROD_ENV;
+
+const OUTPUT_DIR = 'dist';
+
+const config: Configuration = {
+  name: 'via-frontend',
+  mode: isDevMode ? DEV_ENV : PROD_ENV,
+  devtool: isDevMode ? 'inline-source-map' : 'hidden-source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+  },
+  entry: './src/index',
+  target: ['web', 'es5'],
+  module: {
+    rules: [],
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin({ NODE_ENV: isDevMode ? DEV_ENV : PROD_ENV }),
+  ],
+  output: {
+    path: path.join(__dirname, OUTPUT_DIR),
+    filename: '[name].js',
+  },
+};
+
+if (isDevMode) {
+  if (config.plugins) {
+    config.plugins.push(
+      new BundleAnalyzerPlugin({ analyzerMode: 'server', openAnalyzer: false }),
+    );
+  }
+} else {
+  if (config.plugins) {
+    config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'static' }));
+  }
+}
+
+export default config;
