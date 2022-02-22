e7c3739b0d830c8ec446742eceda988c21e1c3c4
Kyeongho Yang <88yangkh@gmail.com>
2022-02-22 15:25:14 +0900

    :lipstick: Grid Mixin 추가

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,3 +1,4 @@
 export * as styles from './variables';
 export * from './themes';
 export * from './GlobalStyles';
+export * as mixins from './mixins';
---
+++ src/styles/mixins.ts
@@ -0,0 +1,17 @@
+import { css } from '@emotion/react';
+import { styles } from '@/styles';
+
+export const grid = (
+  cols = styles.grid.column,
+  rowGap = styles.grid.rowGap,
+  colGap = styles.grid.gutter,
+) => css`
+  width: ${styles.maxWidth}rem;
+  padding: 0 ${colGap / 2}rem;
+  margin: 0 auto;
+
+  display: grid;
+  grid-template-columns: repeat(${cols}, 1fr);
+  row-gap: ${rowGap}rem;
+  column-gap: ${colGap}rem;
+`;
