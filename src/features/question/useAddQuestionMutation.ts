2271771d8fe610c8d9fbdc013e53b839b218c000
Kyeongho Yang <88yangkh@gmail.com>
2022-03-30 01:17:07 +0900

    :hammer: [문의] 문의 등록 성공/실패 시 알림창 출력

--- src/features/question/useAddQuestionMutation.ts
+++ src/features/question/useAddQuestionMutation.ts
@@ -9,6 +9,10 @@ export const useAddQuestionMutation = () => {
   return useMutation(addQuestion, {
     onSuccess: () => {
       queryClient.refetchQueries(QUERY.PRODUCT.QUESTIONS);
+      alert('상품 문의가 등록되었습니다.');
+    },
+    onError: () => {
+      alert('상품 문의 등록 중 오류가 발생했습니다.');
     },
   });
 };
