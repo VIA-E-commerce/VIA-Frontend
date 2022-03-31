c1f26aa55afbc980decca79be0ee20af79f097c9
Kyeongho Yang <88yangkh@gmail.com>
2022-03-31 15:45:52 +0900

    :sparkles: [마이 페이지] 내 문의 목록 탭 추가

--- src/pages/MyPage/MyQuestions/MyQuestions.tsx
+++ src/pages/MyPage/MyQuestions/MyQuestions.tsx
@@ -1,7 +1,68 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+
+import {
+  useMyQuestions,
+  QuestionItem,
+  QuestionEditorModal,
+} from '@/features/question';
+import { GridSection, Pagination, Tab } from '@/components';
+import { QUERY } from '@/constants';
+
+const PAGE_RANGE = 5;
+const PAGE_SIZE = 10;
 
 const MyQuestions = () => {
-  return <div>나의 문의</div>;
+  const [pageNum, setPageNum] = useState(1);
+  const { data: pagination } = useMyQuestions({
+    pageNum,
+    pageSize: PAGE_SIZE,
+  });
+
+  const questions = pagination?.list;
+
+  const [activeQuestionId, setActiveQuestionId] = useState<number | null>(null);
+
+  const handleClickQuestion = useCallback((questionId: number) => {
+    setActiveQuestionId((prev) => {
+      return prev === questionId ? null : questionId;
+    });
+  }, []);
+
+  const onClickPageButton = (pageNum: number) => {
+    setPageNum(pageNum);
+  };
+
+  if (!pagination || !questions) {
+    return <GridSection cols={1}>로딩 중</GridSection>;
+  }
+
+  return (
+    <>
+      <Tab>
+        <div>
+          {questions.map((question) => (
+            <QuestionItem
+              key={question.id}
+              question={question}
+              active={activeQuestionId === question.id}
+              owned
+              onClick={() => handleClickQuestion(question.id)}
+              queryKey={QUERY.USER.MY_QUESTIONS}
+              isForMyPage={true}
+            />
+          ))}
+        </div>
+        <Pagination
+          pageRange={PAGE_RANGE}
+          totalPages={pagination.totalPages}
+          currentPage={pageNum}
+          onClickPageButton={onClickPageButton}
+        />
+      </Tab>
+
+      <QuestionEditorModal queryKey={QUERY.USER.MY_QUESTIONS} />
+    </>
+  );
 };
 
 export default MyQuestions;
