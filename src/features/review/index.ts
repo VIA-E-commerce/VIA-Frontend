d27988268ab7f61dd02e2314bbb046316cd15ed7
Kyeongho Yang <88yangkh@gmail.com>
2022-04-03 07:01:40 +0900

    :necktie: React Query 상품 후기 수정 API & Hook 추가

--- src/features/review/index.ts
+++ src/features/review/index.ts
@@ -1,5 +1,6 @@
 export * from './ProductReviewViewer';
 
+export * from './useEditReviewMutation';
 export * from './useMyReviews';
 export * from './useProductReviews';
 export * from './useRemoveReviewMutation';
