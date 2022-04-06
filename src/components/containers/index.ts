40987967abbcff481eb3de85331687b687c09738
Kyeongho Yang <88yangkh@gmail.com>
2022-04-07 07:52:08 +0900

    :truck: 공통 컴포넌트 containers, scroll 분류 추가

---
+++ src/components/containers/index.ts
@@ -0,0 +1,4 @@
+export * from './Empty';
+export * from './LabelField';
+export * from './LabelSection';
+export * from './Loading';
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,15 +1,12 @@
-export * from './Empty';
-export * from './LabelField';
-export * from './LabelSection';
-export * from './Loading';
 export * from './Pagination';
 export * from './PriceLabel';
-export * from './ScrollToTop';
 export * from './SNSProviderIcon';
 
 export * from './buttons';
+export * from './containers';
 export * from './inputs';
 export * from './layouts';
 export * from './modals';
+export * from './scroll';
 export * from './seo';
 export * from './tab';
