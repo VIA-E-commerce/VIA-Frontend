ff29691c7b954a499e907d5ff17c5775986970e4
Kyeongho Yang <88yangkh@gmail.com>
2022-04-07 07:45:49 +0900

    :lipstick: DropDown Hover 시 생기는 흰 줄 제거

--- src/components/buttons/DropDown/DropDown.styles.tsx
+++ src/components/buttons/DropDown/DropDown.styles.tsx
@@ -18,20 +18,27 @@ export const Wrapper = styled.div`
     list-style: none;
     padding: 0;
 
-    background: ${({ theme }) => theme.color.background};
     width: 10rem;
     border: ${styles.border.level1}rem solid ${({ theme }) => theme.color.font};
 
     position: absolute;
-
     display: none;
+
+    &,
+    li {
+      background: ${({ theme }) => theme.color.background};
+    }
+
+    &:hover,
+    li:hover {
+      background: ${({ theme }) => theme.color.font};
+    }
 
     li {
       text-align: center;
-      padding: ${styles.space.level1}rem;
+      padding: ${styles.space.level2}rem ${styles.space.level1}rem;
 
       &:hover {
-        background: ${({ theme }) => theme.color.font};
         color: ${({ theme }) => theme.color.fontReverse};
 
         a {
