8b5303050866fb0095c9505fd4be6ac25a0d2b20
Kyeongho Yang <88yangkh@gmail.com>
2022-04-07 04:56:23 +0900

    :hammer: useScript 훅 추가 : <script> 태그를 사용하기 위한 Hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,3 +2,4 @@ export * from './useDetailTabPageButton';
 export * from './useDocumentScroll';
 export * from './useNumberInput';
 export * from './useProtectedFunction';
+export * from './useScript';
