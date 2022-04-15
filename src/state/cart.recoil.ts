4d36f580b39ed81af9e3468f36c96d42379f27da
Kyeongho Yang <88yangkh@gmail.com>
2022-04-15 21:44:11 +0900

    :necktie: Recoil 장바구니 아이템 개수 Atom 추가

--- src/state/cart.recoil.ts
+++ src/state/cart.recoil.ts
@@ -2,6 +2,11 @@ import { atom } from 'recoil';
 
 import { CartItemResponse } from '@/types';
 
+export const cartItemCountState = atom<number | undefined>({
+  key: 'cartItemCountState',
+  default: undefined,
+});
+
 export const selectedCartItemsState = atom({
   key: 'selectedCartItemsState',
   default: new Map<number, CartItemResponse>(),
