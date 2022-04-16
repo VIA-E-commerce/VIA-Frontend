61e737f367fd19399342aa8543c3e38397972c76
Kyeongho Yang <88yangkh@gmail.com>
2022-04-17 08:32:58 +0900

    :lipstick: [공통] TextArea 포커스 시 outline 얇게 수정

--- src/components/inputs/TextArea/TextArea.styles.tsx
+++ src/components/inputs/TextArea/TextArea.styles.tsx
@@ -6,10 +6,16 @@ export const StyledTextArea = styled.textarea`
   width: 100%;
   height: 100%;
   padding: ${styles.space.level4}rem;
+
   resize: none;
 
   border: ${styles.border.level1}rem solid
     ${({ theme }) => theme.color.inputBorder};
 
   font-size: ${styles.fontSize.normal}rem;
+
+  &:focus {
+    outline: ${styles.border.level1}rem solid
+      ${({ theme }) => theme.color.black};
+  }
 `;
